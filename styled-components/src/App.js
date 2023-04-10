20d4a29e8f67940a15cf116c0c717ea09100a5e0
subin <subin9192@gmail.com>
2023-04-11 08:23:52 +0900

    Dialog

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const AppBlock = styled.div`
   width: 512px;
@@ -23,32 +24,46 @@ const palette = {
 };
 
 function App() {
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+
   return (
     <ThemeProvider
       theme={{
         palette,
       }}
     >
-      <AppBlock>
-        <ButtonGroup>
-          <Button color="pink" size="large">
-            BUTTON
-          </Button>
-          <Button>BUTTON</Button>
-          <Button color="gray" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="pink" size="large" outline>
-            BUTTON
-          </Button>
-          <Button outline>BUTTON</Button>
-          <Button color="gray" size="small" outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+      <>
+        <AppBlock>
+          <ButtonGroup>
+            <Button color="pink" size="large">
+              BUTTON
+            </Button>
+            <Button>BUTTON</Button>
+            <Button color="gray" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="large" outline>
+              BUTTON
+            </Button>
+            <Button outline>BUTTON</Button>
+            <Button color="gray" size="small" outline>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 }
