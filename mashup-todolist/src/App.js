ab67ac4bd02d41ea51f5bbfaa6fcc0bbf45e0fec
subin <subin9192@gmail.com>
2023-04-25 07:10:19 +0900

    TodoList UI

--- mashup-todolist/src/App.js
+++ mashup-todolist/src/App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
 function App() {
-  return <div>안녕하세요!</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>안녕하세요!</div>;
+    </>
+  );
 }
 
 export default App;
