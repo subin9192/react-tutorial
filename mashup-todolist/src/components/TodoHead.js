11ff958b1792e47e3ea5b385867554fa40977fd6
subin <subin9192@gmail.com>
2023-04-27 08:32:54 +0900

    TodoList UI

---
+++ mashup-todolist/src/components/TodoHead.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1px solid #e9ecef;
+
+  h1 {
+  }
+  .day {
+  }
+  .tasks-left {
+  }
+`;
+
+function TodoHead() {
+  return <div></div>;
+}
+
+export default TodoHead;
