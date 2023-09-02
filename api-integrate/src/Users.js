181c5d65cdec8d128908afe5608bc0fbf1beb80b
unknown <vercel@kakao.com>
2023-09-02 17:34:48 +0900

    feat: Update

---
+++ api-integrate/src/Users.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Users() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setUsers(null);
+        setError(null);
+        setLoading(true);
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users/"
+        );
+      } catch (e) {}
+    };
+  }, []);
+
+  return <div />;
+}
+
+export default Users;
